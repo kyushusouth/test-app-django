c7afb4cede6b5fc900044b3b0e42dde979e9800c
Taisuke Minami <tmnm13009@gmail.com>
2024-03-20 08:10:39 +0900

    nullとblankは初期値でよく、冗長なので省いた

--- mysite/main_app/models.py
+++ mysite/main_app/models.py
@@ -8,22 +8,25 @@ from django.db import models
 
 class Respondents(models.Model):
     created_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=36, unique=True, null=False, blank=False)
-    sex = models.CharField(max_length=1, null=False, blank=False)
-    age = models.SmallIntegerField(null=False, blank=False)
+    name = models.CharField(max_length=36, unique=True)
+    sex = models.CharField(max_length=1)
+    age = models.SmallIntegerField()
 
 
 class SampleMetaData(models.Model):
     file_path = models.FileField(upload_to="")
-    file_name = models.CharField(max_length=36, unique=True, null=False, blank=False)
-    speaker_name = models.CharField(max_length=100, null=False, blank=False)
-    model_name = models.CharField(max_length=100, null=False, blank=False)
-    sample_name = models.CharField(max_length=100, null=False, blank=False)
-    kind = models.CharField(max_length=100, null=False, blank=False)
+    file_name = models.CharField(max_length=36, unique=True)
+    speaker_name = models.CharField(max_length=100)
+    model_name = models.CharField(max_length=100)
+    sample_name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.file_path.url
 
 
 class Answers(models.Model):
     respondent = models.ForeignKey(Respondents, on_delete=models.CASCADE)
     sample_meta_data = models.ForeignKey(SampleMetaData, on_delete=models.CASCADE)
-    naturalness = models.SmallIntegerField(null=False, blank=False)
-    intelligibility = models.SmallIntegerField(null=False, blank=False)
+    naturalness = models.SmallIntegerField()
+    intelligibility = models.SmallIntegerField()
