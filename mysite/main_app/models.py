58582c899125189ea75af00a4a340689d5c6318f
Taisuke Minami <tmnm13009@gmail.com>
2024-03-21 19:22:03 +0900

    DBスキーマの見直し

--- mysite/main_app/models.py
+++ mysite/main_app/models.py
@@ -1,32 +1,49 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class CustomUser(AbstractUser):
-#     sex = models.CharField(max_length=1, null=False, blank=False)
-#     age = models.SmallIntegerField(null=False, blank=False)
+class Sex(models.Model):
+    kind = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.kind
 
 
-class Respondents(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=36, unique=True)
-    sex = models.CharField(max_length=1)
-    age = models.SmallIntegerField()
+class Respondents(AbstractUser):
+    sex = models.ForeignKey(Sex, on_delete=models.CASCADE, null=True, blank=True)
+    age = models.SmallIntegerField(null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.username
 
 
 class SampleMetaData(models.Model):
-    file_path = models.FileField(upload_to="")
-    file_name = models.CharField(max_length=36, unique=True)
+    file_path = models.FileField(upload_to="", unique=True)
     speaker_name = models.CharField(max_length=100)
     model_name = models.CharField(max_length=100)
     sample_name = models.CharField(max_length=100)
     kind = models.CharField(max_length=100)
-    
+
     def __str__(self) -> str:
         return self.file_path.url
+
+
+class NaturalnessItems(models.Model):
+    item = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.pk}: {self.item}"
+
+
+class IntelligibilityItems(models.Model):
+    item = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.pk}: {self.item}"
 
 
 class Answers(models.Model):
     respondent = models.ForeignKey(Respondents, on_delete=models.CASCADE)
     sample_meta_data = models.ForeignKey(SampleMetaData, on_delete=models.CASCADE)
-    naturalness = models.SmallIntegerField()
-    intelligibility = models.SmallIntegerField()
+    naturalness = models.ForeignKey(NaturalnessItems, on_delete=models.CASCADE)
+    intelligibility = models.ForeignKey(IntelligibilityItems, on_delete=models.CASCADE)
