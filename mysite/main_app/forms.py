8350b80a0ff39fd790be3c6aaec54e9d91f06c1e
Taisuke Minami <tmnm13009@gmail.com>
2024-03-21 19:21:40 +0900

    新規ユーザー登録フォームと、評価実験回答フォームを実装

--- mysite/main_app/forms.py
+++ mysite/main_app/forms.py
@@ -1,48 +1,28 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm, RadioSelect
 
-from .models import SampleMetaData
-
-
-class UserForm(forms.Form):
-    username = forms.CharField(label="Your name", max_length=100)
-    email = forms.CharField(
-        label="Email", max_length=100, widget=forms.widgets.EmailInput
-    )
-    password = forms.CharField(label="Password", max_length=100)
+from .models import Answers, Respondents
 
 
-class AnswerForm(forms.Form):
-    naturalness = forms.fields.ChoiceField(
-        label="naturalness",
-        choices=(
-            (1, "1: 非常に悪い"),
-            (2, "2: 悪い"),
-            (3, "3: 普通"),
-            (4, "4: 良い"),
-            (5, "5: 非常に良い"),
-        ),
-        required=True,
-        
-        initial="",
-        widget=forms.widgets.RadioSelect,
-    )
-    intelligibility = forms.fields.ChoiceField(
-        label="intelligibility",
-        choices=(
-            (1, "1: 非常に悪い"),
-            (2, "2: 悪い"),
-            (3, "3: 普通"),
-            (4, "4: 良い"),
-            (5, "5: 非常に良い"),
-        ),
-        required=True,
-        initial="",
-        widget=forms.widgets.RadioSelect,
-    )
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = Respondents
+        fields = ["username"]
+
+
+class EvaluationForm(ModelForm):
     url = forms.fields.CharField(
         label="url",
         required=False,
         initial="",
         widget=forms.widgets.HiddenInput,
     )
-    samplemetadata = forms.ModelChoiceField(queryset=SampleMetaData.objects.all())
+
+    class Meta:
+        model = Answers
+        fields = ["naturalness", "intelligibility"]
+        widgets = {
+            "naturalness": RadioSelect,
+            "intelligibility": RadioSelect,
+        }
