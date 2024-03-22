0dbc3822db46d8592c10e2b34550f7faf3778032
Taisuke Minami <tmnm13009@gmail.com>
2024-03-23 00:17:26 +0900

    ルーティング修正

--- mysite/main_app/urls.py
+++ mysite/main_app/urls.py
@@ -4,21 +4,14 @@ from django.urls import path
 from . import views
 
 app_name = "main_app"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("eval", views.eval, name="eval"),
-    path("thanks", views.thanks, name="thanks"),
-    path(
-        "check_respondents",
-        views.CheckRespondentsView.as_view(),
-        name="check_respondents",
-    ),
-    path("check_answers", views.CheckAnswersView.as_view(), name="check_answers"),
-    path("user_page", views.user_page, name="user_page"),
     path(
         "login",
         auth_views.LoginView.as_view(
-            template_name="main_app/login.html", next_page="main_app:user_page"
+            template_name="main_app/login.html",
+            next_page="main_app:index",
         ),
         name="login",
     ),
@@ -27,10 +20,17 @@ urlpatterns = [
         auth_views.LogoutView.as_view(next_page="main_app:login"),
         name="logout",
     ),
-    path("signup", views.SignupView.as_view(), name="signup"),
     path(
         "answer_respondent_info/<int:pk>",
         views.RespondentInfoCreateView.as_view(),
         name="answer_respondent_info",
     ),
+    path("eval", views.eval, name="eval"),
+    path("signup", views.SignupView.as_view(), name="signup"),
+    path(
+        "check_respondents",
+        views.CheckRespondentsView.as_view(),
+        name="check_respondents",
+    ),
+    path("check_answers", views.CheckAnswersView.as_view(), name="check_answers"),
 ]
